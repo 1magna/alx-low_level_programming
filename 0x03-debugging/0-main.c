b43d3b196f966236d8c56437d1690271d48b9bbd
1magna <ihekaolivia@gmail.com>
2022-09-16 22:00:49 -0700

    hard thing

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints positive or negative integer
+ * 
+ * Return: 0
+ *
+ * */
+
+int main(void)
+
+{
+	int i;
+	i = 0;
+
+	positive_ornegative(i);
+
+	return(0);
+
+}	
